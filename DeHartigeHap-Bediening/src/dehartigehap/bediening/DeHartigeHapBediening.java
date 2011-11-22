439add1d8a9635debdcd0db2a6d7f8a92d97fb68
peterrus <petorrus@gmail.com>
2011-11-22 21:37:38 +0100

    initieel project

---
+++ DeHartigeHap-Bediening/src/dehartigehap/bediening/DeHartigeHapBediening.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dehartigehap.bediening;
+
+/**
+ *
+ * @author peterrus
+ */
+public class DeHartigeHapBediening {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
